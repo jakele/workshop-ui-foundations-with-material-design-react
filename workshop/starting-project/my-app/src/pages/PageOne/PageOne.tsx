d494b25c516366a062bc2889160044ee70b4ab7e
Jeffrey Kern <jeffkernsemail@gmail.com>
2023-06-13 07:57:41 -0500

    reverting starting project part 1 to begining

--- workshop/starting-project/my-app/src/pages/PageOne/PageOne.tsx
+++ workshop/starting-project/my-app/src/pages/PageOne/PageOne.tsx
@@ -3,31 +3,27 @@ import { Box, Container, Grid, Paper, Typography } from "@mui/material";
 function PageOne() {
   return (
     <>
+      <Container maxWidth="sm">
+        <Box sx={{ bgcolor: "#cfe8fc", height: "30vh" }} />
+      </Container>
+      <Container maxWidth="md">
+        <Box sx={{ bgcolor: "#cfe8fc", height: "30vh" }} />
+      </Container>
       <Container maxWidth="lg">
         <Grid container spacing={2}>
-          <Grid item md={12}>
-              <Typography variant="h1" gutterBottom>Lorem ipsum dolor sit amet consectetur</Typography>
+          <Grid item xs={8}>
+            <Paper>
+              <Typography variant="body1">xs=8</Typography>
+            </Paper>
           </Grid>
-          <Grid item md={4}>
-              <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, itaque accusamus blanditiis dolorem corrupti numquam quas ipsam ad alias accusantium? Neque cupiditate ipsa corporis, minus fugiat deserunt labore nam nobis?</Typography>
+          <Grid item xs={4}>
+            <Paper><Typography variant="caption">xs=4</Typography></Paper>
           </Grid>
-          <Grid item md={4}>
-              <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, itaque accusamus blanditiis dolorem corrupti numquam quas ipsam ad alias accusantium? Neque cupiditate ipsa corporis, minus fugiat deserunt labore nam nobis?</Typography>
+          <Grid item xs={4}>
+            <Paper><Typography variant="h1">xs=4</Typography></Paper>
           </Grid>
-          <Grid item md={4}>
-              <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, itaque accusamus blanditiis dolorem corrupti numquam quas ipsam ad alias accusantium? Neque cupiditate ipsa corporis, minus fugiat deserunt labore nam nobis?</Typography>
-          </Grid>
-          <Grid item md={12}>
-              <Typography variant="h2" gutterBottom>Lorem ipsum dolor sit amet consectetur</Typography>
-          </Grid>
-          <Grid item md={4}>
-              <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, itaque accusamus blanditiis dolorem corrupti numquam quas ipsam ad alias accusantium? Neque cupiditate ipsa corporis, minus fugiat deserunt labore nam nobis?</Typography>
-          </Grid>
-          <Grid item md={4}>
-              <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, itaque accusamus blanditiis dolorem corrupti numquam quas ipsam ad alias accusantium? Neque cupiditate ipsa corporis, minus fugiat deserunt labore nam nobis?</Typography>
-          </Grid>
-          <Grid item md={4}>
-              <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, itaque accusamus blanditiis dolorem corrupti numquam quas ipsam ad alias accusantium? Neque cupiditate ipsa corporis, minus fugiat deserunt labore nam nobis?</Typography>
+          <Grid item xs={8}>
+            <Paper><Typography variant="h2">xs=8</Typography></Paper>
           </Grid>
         </Grid>
       </Container>
